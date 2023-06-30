7ae5060ff6ff845c6a847dae5cb1683a8c2fb5d1
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2023-06-30 18:11:38 +0600

    main

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,44 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[400],
       ),
-      body: Center(
-        child: FilledButton(
-          onPressed: () {
-            print('button e guta disos kn');
-          },
-          child: Text('button'),
-        ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 4,
+              child: Image.asset('assets/hmm.jpg')
+          ),
+          Expanded(
+            flex: 10,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.red,
+              child: Text('one'),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              color: Colors.green,
+              padding: EdgeInsets.all(30.0),
+              child: Text('two'),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(
+              color: Colors.amber,
+              padding: EdgeInsets.all(30.0),
+              child: Text('three'),
+            ),
+          ),
+        ],
       ),
-    );
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('guta dis na sagol');
+        },
+        child: Text('hehe'),
+      ),
+      );
   }
 }
